bca02060975d04ff58bea6841c7d8ca8918d4751
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-18 17:57:34 -0800

    Added the manual handler.

--- src/handlers/freshHandler.js
+++ src/handlers/freshHandler.js
@@ -13,31 +13,39 @@ export async function freshHandler(db_paths, list_projects, list_assets, list_ve
         createTables(db);
         db_handles[k] = db;
     }
-    const to_extract = Object.keys(db_handles);
 
     const all_projects = await list_projects();
-    for (const proj of all_projects) {
-        const all_assets = await list_assets(proj);
-        for (const ass of all_assets) {
-            const latest = find_latest(proj, ass);
-            if (latest == null) {
-                continue;
-            }
-
-            const all_versions = await list_versions(proj, ass);
-            for (const ver of all_versions) {
-                const summ = await read_summary(proj, ass, ver);
-                if ("on_probation" in summ && summ.on_probation) {
-                    continue;
-                }
-
-                const output = await read_metadata(proj, ass, ver, to_extract);
-                for (const e of to_extract) {
-                    addVersion(db_handles[e], proj, ass, ver, (latest == ver), output[e], tokenizable);
-                }
-            }
-        }
+    for (const project of all_projects) {
+        await internal_freshProject(db_handles, project, list_assets, list_versions, find_latest, read_summary, read_metadata, tokenizable);
     }
+}
 
-    return;
+// Only exported for the purpose of re-use in manualHandler.js.
+export async function internal_freshProject(db_handles, project, list_assets, list_versions, find_latest, read_summary, read_metadata, tokenizable) {
+    const all_assets = await list_assets(project);
+    for (const asset of all_assets) {
+        await internal_freshAsset(db_handles, project, asset, list_versions, find_latest, read_summary, read_metadata, tokenizable);
+    }
+}
+
+export async function internal_freshAsset(db_handles, project, asset, list_versions, find_latest, read_summary, read_metadata, tokenizable) {
+    const latest = find_latest(project, asset);
+    if (latest == null) { // short-circuit if latest=null, as that means that there are no non-probational versions.
+        return; 
+    }
+    const all_versions = await list_versions(project, asset);
+    for (const version of all_versions) {
+        await internal_freshVersion(db_handles, project, asset, version, latest, read_summary, read_metadata, tokenizable);
+    }
+}
+
+export async function internal_freshVersion(db_handles, project, asset, version, latest, read_summary, read_metadata, tokenizable) {
+    const summ = await read_summary(project, asset, version);
+    if ("on_probation" in summ && summ.on_probation) {
+        return;
+    }
+    const output = await read_metadata(project, asset, version, Object.keys(db_handles));
+    for (const [e, db] of Object.entries(db_handles)) {
+        addVersion(db, project, asset, version, (latest == version), output[e], tokenizable);
+    }
 }
